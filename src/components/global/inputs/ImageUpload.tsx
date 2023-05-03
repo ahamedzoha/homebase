c1f207715b04039de81a2ba09d74a1202f538f39
ahamedzoha <ahamed.zoha@gmail.com>
2023-05-03 18:36:57 +0600

    wip setting up image upload usinf cloudinary

--- src/components/global/inputs/ImageUpload.tsx
+++ src/components/global/inputs/ImageUpload.tsx
@@ -1,10 +1,69 @@
 "use client"
+import { FC, useCallback } from "react"
+import { CldUploadWidget } from "next-cloudinary"
+import Image from "next/image"
+import { TbPhotoPlus } from "react-icons/tb"
 
-import { FC } from "react"
+declare global {
+  var Cloudinary: any
+}
 
-interface ImageUploadProps {}
-const ImageUpload: FC<ImageUploadProps> = () => {
-  return <div>Enter</div>
+interface ImageUploadProps {
+  onChange: (value: string) => void
+  value: string
+}
+const ImageUpload: FC<ImageUploadProps> = ({ onChange, value }) => {
+  const handleUpload = useCallback(
+    (error: any, result: any) => {
+      onChange(result.info.secure_url)
+    },
+    [onChange]
+  )
+
+  return (
+    <CldUploadWidget
+      onUpload={handleUpload}
+      uploadPreset=""
+      options={{
+        maxFiles: 1,
+      }}
+    >
+      {({ open }) => (
+        <div
+          onClick={() => open?.()}
+          className="
+            relative 
+            cursor-pointer 
+            hover:opacity-70 
+            transition 
+            border-dashed 
+            border-2 
+            border-neutral-300 
+            p-20 
+            flex 
+            flex-col 
+            justify-center 
+            items-center
+            gap-4
+            text-neutral-600
+            "
+        >
+          <TbPhotoPlus size={50} />
+          <div className="font-semibold text-lg">Click to upload</div>
+          {value && (
+            <div className="absolute inset-0 w-full h-full">
+              <Image
+                alt="upload"
+                fill
+                style={{ objectFit: "cover" }}
+                src={value}
+              />
+            </div>
+          )}
+        </div>
+      )}
+    </CldUploadWidget>
+  )
 }
 
 export default ImageUpload
